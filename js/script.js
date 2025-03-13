d9ad76fbf3dc67ba0fe4e3ad09816b69fd905d9d
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-03-13 14:56:25 +0100

    feat: generate emails and show in html

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const listElement = document.getElementById('list-email');
+const numEmails = 10;
+
+for (let i = 0; i < numEmails; i++) {
+    createEmail();
+}
+
+function createEmail() {
+    let endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+    axios.get(endpoint)
+        .then(response => {
+            const result = response.data;
+            let email = result.response;
+            renderHTML(listElement, email);
+        });
+}
+
+function renderHTML(parent, elements) {
+    let liElement = document.createElement('li');
+    liElement.innerText = elements;
+    parent.appendChild(liElement);
+}
+
